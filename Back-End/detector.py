fcad1837ecf5ebc11632c5e396ed32469a22c348
Irenna Lumbuun <irennanicole1@gmail.com>
2020-05-23 17:55:01 -0700

    draw past locations

--- Back-End/detector.py
+++ Back-End/detector.py
@@ -1,8 +1,8 @@
 # todo
-# 0. Access Camera
-# 1. detect hand
-# 2. save movement in a deque (is queue better?)
-# 3. draw a line on hand indicating hand is detected
+# 0. ok - Access Camera
+# 1. ok - detect hand
+# 2. ok - save movement in a deque (is queue better?)
+# 3. ok - draw a line on hand indicating hand is detected
 # 4. connect to flask so it's displayed on the web
 # 5. render video guide
 # 6. detect hand on that video guide
@@ -13,16 +13,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+from collections import deque
 
 def nothing(x):
     pass
 
-def maskImage(frame, lh, ls, lv, uh, us, uv):
+def maskImage(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #tried this using trackbar on a daylight
-    #not sure if it's correct
-    low_bound = np.array([75, 43, 0])
-    high_bound = np.array([129, 101, 208])
+
+    lh = cv2.getTrackbarPos('LH', 'Tracking')
+    ls = cv2.getTrackbarPos('LS', 'Tracking')
+    lv = cv2.getTrackbarPos('LV', 'Tracking')
+
+    uh = cv2.getTrackbarPos('UH', 'Tracking')
+    us = cv2.getTrackbarPos('US', 'Tracking')
+    uv = cv2.getTrackbarPos('UV', 'Tracking')
 
     low_bound = np.array([lh, ls, lv])
     high_bound = np.array([uh, us, uv])
@@ -33,18 +38,10 @@ def maskImage(frame, lh, ls, lv, uh, us, uv):
     mask = cv2.dilate(mask, None, iterations=2)
     return mask
 
-# try with canny detection
-def getCanny(frame, th1, th2):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canny = cv2.Canny(frame, th1, th2, apertureSize=3)
-    print("length canny =" , len(canny))
-    return canny
-
 #try with getcontour
 def getContour(frame):
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     contours, hierarchy = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    print(len(contours))
     return contours
 
 # Main:
@@ -69,6 +66,9 @@ def Main():
     cv2.createTrackbar('US', 'Tracking', 101, 255, nothing)
     cv2.createTrackbar('UV', 'Tracking', 208, 255, nothing)
     
+    hand1_loc = deque(maxlen= 100)
+    hand2_loc = deque(maxlen= 100)
+
     while True:
         ret, frame = vs.read()
         if not ret:
@@ -79,29 +79,49 @@ def Main():
 
         # TRY - canny edge detection // contour detection
         minArea = cv2.getTrackbarPos('minArea', 'Tracking')
-        lh = cv2.getTrackbarPos('LH', 'Tracking')
-        ls = cv2.getTrackbarPos('LS', 'Tracking')
-        lv = cv2.getTrackbarPos('LV', 'Tracking')
 
-        uh = cv2.getTrackbarPos('UH', 'Tracking')
-        us = cv2.getTrackbarPos('US', 'Tracking')
-        uv = cv2.getTrackbarPos('UV', 'Tracking')
-
-        mask = maskImage(frame, lh, ls, lv, uh, us, uv)
-         #clean frame
-        mask = cv2.erode(mask, None, iterations =2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        #canny = getCanny(frame, th1, th2)
+        mask = maskImage(frame)
         contour = getContour(mask)
 
         #cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
+        index = 0
         for c in contour:
-            (x, y, w, h) = cv2.boundingRect(c)
+            #has to be bigger than the minimum area of a hand
             if cv2.contourArea(c) < minArea:
                 continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            # show that hand is detected
             cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, "Detected: hand", (x -10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-    
+            
+            moment = cv2.moments(c)
+            center = (int(moment["m10"] / moment["m00"]), int (moment["m01"] / moment["m00"]))
+            #store upper x, y, w, h
+            if index == 0:
+                hand1_loc.appendleft([x, y, w, h, center])
+                cv2.putText(frame, "Detected: hand1", (x -10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            if index == 1:
+                hand2_loc.appendleft([x, y, w, h, center])
+                cv2.putText(frame, "Detected: hand2", (x -10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+            index +=1
+            
+        
+        #draw movement
+        maxLength = len(hand1_loc) if len(hand1_loc) > len(hand2_loc) else len(hand2_loc)
+
+        for i in range (1, maxLength):
+            #draw hand1 location
+            if hand1_loc[i-1] is None or hand1_loc[i] is None:
+                continue
+
+            thickness = int(np.sqrt(100/ float(i + 1)) * 4)
+            cv2.line(frame, hand1_loc[i-1][4], hand1_loc[i][4], (0, 0, 255), thickness)
+
+            #draw hand2 location
+            if len(hand2_loc)> i:
+                if hand2_loc[i-1] is None or hand2_loc[i] is None:
+                    continue
+                thickness = int(np.sqrt(100/ float(i + 1)) * 4)
+                cv2.line(frame, hand2_loc[i-1][4], hand2_loc[i][4], (0, 255, 0), thickness)
 
         cv2.imshow('Contour', frame)
 
